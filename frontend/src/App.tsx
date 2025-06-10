a4c52c5f1bdd94d4b9cb2c955efb19196f1cae98
Michał Pętlewski <dachloo@wp.pl>
2025-06-10 15:01:23 +0200

    configured frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+
+function App() {
+
+
+  return (
+    <>
+    <h1 className="text-3xl font-bold underline red">
+      Hello world!
+    </h1>
+    <span className='text-red-100'>
+dsada
+    </span>
+    </>
+  )
+}
+
+export default App
